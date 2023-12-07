677f39b552a56f5d25f8effd106d7cdb587023f7
carlosberserker01 <carlosberserker01@gmail.com>
2023-12-07 15:31:55 -0600

    UPDATE ES7 class components sintaxis

--- src/components/Eventos.jsx
+++ src/components/Eventos.jsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 
-export default class Eventos extends Component {
+export class EventosES6 extends Component {
   constructor(props){
     super(props)
     this.state = {
@@ -34,7 +34,48 @@ export default class Eventos extends Component {
   render () {
     return (
       <div>
-        <h2>Eventos en Componentes de Clase</h2>
+        <h2>Eventos en Componentes de Clase ES6</h2>
+        <h3>{this.state.contador}</h3>
+        <nav>
+          <button onClick={this.sumar}>+</button>
+          <button onClick={this.restar}>-</button>
+        </nav>
+        <button onClick={this.reset}>Reset</button>
+      </div>
+    )
+  }
+}
+
+// Properties Initializer
+export class EventosES7 extends Component {
+  state = {
+    contador: 0
+  }
+
+  //Arrow functions
+  sumar = (e) => {
+    console.log(this);
+    this.setState({
+      contador: this.state.contador + 1
+    })
+  }
+
+  restar = (e) => {
+    this.setState({
+      contador: this.state.contador - 1
+    })
+  }
+
+  reset = (e) => {
+    this.setState({
+      contador: 0
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h2>Eventos en Componentes de Clase ES7</h2>
         <h3>{this.state.contador}</h3>
         <nav>
           <button onClick={this.sumar}>+</button>
