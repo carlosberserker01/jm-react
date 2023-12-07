5be77da04342b7aa6fe6a4d589e0f9d49871fd42
carlosberserker01 <carlosberserker01@gmail.com>
2023-12-07 15:53:51 -0600

    UPDATE native, sintetic & custom events

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Propiedades from './components/Propiedades'
 import Estado from './components/Estado'
 import RenderizadoCondicional from './components/RenderizadoCondicional'
 import RenderizadoElementos from './components/RenderizadoElementos'
-import { EventosES6, EventosES7 } from './components/Eventos'
+import { EventosES6, EventosES7, MasSobreEventos } from './components/Eventos'
 import './App.css'
 
 function App() {
@@ -66,6 +66,8 @@ function App() {
       <EventosES6 />
       <hr />
       <EventosES7 />
+      <hr />
+      <MasSobreEventos />
     </>
   )
 }
