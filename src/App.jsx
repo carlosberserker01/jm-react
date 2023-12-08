2a595d7f227c09463d74f897f40c314b860655f8
carlosberserker01 <carlosberserker01@gmail.com>
2023-12-08 11:57:13 -0600

    UPDATE 13. components life cycle

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import RenderizadoCondicional from './components/RenderizadoCondicional'
 import RenderizadoElementos from './components/RenderizadoElementos'
 import { EventosES6, EventosES7, MasSobreEventos } from './components/Eventos'
 import ComunicacionComponentes from './components/ComunicacionComponentes'
+import CicloVida from './components/CicloVida'
 import './App.css'
 
 function App() {
@@ -71,6 +72,8 @@ function App() {
       <MasSobreEventos />
       <hr />
       <ComunicacionComponentes />
+      <hr />
+      <CicloVida />
     </>
   )
 }
